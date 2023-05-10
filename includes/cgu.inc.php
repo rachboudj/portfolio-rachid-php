a57a545663cf7b57fc15bdcf676b84b6a6b2bdcd
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-05-10 08:44:59 +0200

    ajout de la page CGU

---
+++ includes/cgu.inc.php
@@ -0,0 +1,104 @@
+<h1>Définitions</h1>
+
+<a href="./asset/pdf/cgu.pdf" target='new'>Ouverture et imp du fichier cguunblog.pdf</a>
+
+<p><b>Client :</b> tout professionnel ou personne physique capable au sens des articles 1123 et suivants du Code civil, ou personne morale, qui visite le Site objet des présentes conditions générales.<br>
+    <b>Prestations et Services :</b> <a href="https://unblog.fr">https://unblog.fr</a> met à disposition des Clients :
+</p>
+
+<p><b>Contenu :</b> Ensemble des éléments constituants l’information présente sur le Site, notamment textes – images – vidéos.</p>
+
+<p><b>Informations clients :</b> Ci après dénommé « Information (s) » qui correspondent à l’ensemble des données personnelles susceptibles d’être détenues par <a href="https://unblog.fr">https://unblog.fr</a> pour la gestion de votre compte, de la gestion de la relation client et à des fins d’analyses et de statistiques.</p>
+
+<p><b>Utilisateur :</b> Internaute se connectant, utilisant le site susnommé.</p>
+<p><b>Informations personnelles :</b> « Les informations qui permettent, sous quelque forme que ce soit, directement ou non, l'identification des personnes physiques auxquelles elles s'appliquent » (article 4 de la loi n° 78-17 du 6 janvier 1978).</p>
+<p>Les termes « données à caractère personnel », « personne concernée », « sous traitant » et « données sensibles » ont le sens défini par le Règlement Général sur la Protection des Données (RGPD : n° 2016-679)</p>
+
+<h2>1. Présentation du site internet.</h2>
+<p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique, il est précisé aux utilisateurs du site internet <a href="https://unblog.fr">https://unblog.fr</a> l'identité des différents intervenants dans le cadre de sa réalisation et de son suivi:
+</p>
+<p><strong>Propriétaire</strong> : un influenceur<br>
+
+    <strong>Responsable publication</strong> : contact – contact@unblog.fr<br>
+    Le responsable publication est une personne physique ou une personne morale.<br>
+    <strong>Webmaster</strong> : THE MASTER – contact@unblog.fr<br>
+    <strong>Hébergeur</strong> : 1and1 – 7 Place de la Gare 57200 Sarreguemines 0970 808 911<br>
+    <strong>Délégué à la protection des données</strong> : conatct – contact@alibobo.com<br>
+</p>
+
+<div ng-bind-html="linkHTML">
+    <p>Ces mentions légales RGPD sont issues du <a href="https://fr.orson.io/1371/generateur-mentions-legales" title="générateur gratuit offert par Orson.io">générateur gratuit offert par Orson.io</a></p>
+</div>
+
+
+
+<h2>2. Conditions générales d’utilisation du site et des services proposés.</h2>
+
+<p>Le Site constitue une œuvre de l’esprit protégée par les dispositions du Code de la Propriété Intellectuelle et des Réglementations Internationales applicables.
+    Le Client ne peut en aucune manière réutiliser, céder ou exploiter pour son propre compte tout ou partie des éléments ou travaux du Site.</p>
+
+<p>L’utilisation du site <a href="https://unblog.fr">UnBlog</a> implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment, les utilisateurs du site <a href="https://www.alibobo">UnBlog</a> sont donc invités à les consulter de manière régulière.</p>
+
+<p>Ce site internet est normalement accessible à tout moment aux utilisateurs. Une interruption pour raison de maintenance technique peut être toutefois décidée par <a href="https://www.alibobo">UnBlog</a>, qui s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de l’intervention.
+    Le site web <a href="https://unblog.fr">hUnBlog/a> est mis à jour régulièrement par <a href="https://unblog.fr">UnBlog</a> responsable. De la même façon, les mentions légales peuvent être modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
+
+<h2>3. Description des services fournis.</h2>
+
+<p>Le site internet <a href="https://unblog.fr">UnBlog</a> a pour objet de fournir une information concernant l’ensemble des activités de la société.
+    <a href="https://unblog.fr">UnBlog</a> s’efforce de fournir sur le site <a href="https://unblog.fr">UnBlog</a> des informations aussi précises que possible. Toutefois, il ne pourra être tenu responsable des oublis, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations.
+</p>
+
+<p>Toutes les informations indiquées sur le site <a href="https://unblog.fr">UnBlog</a> sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les renseignements figurant sur le site <a href="https://unblog.fr">UnBlog</a> ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne.</p>
+
+<h2>4. Limitations contractuelles sur les données techniques.</h2>
+
+<p>Le site utilise la technologie JavaScript.
+
+    Le site Internet ne pourra être tenu responsable de dommages matériels liés à l’utilisation du site. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour
+    Le site <a href="https://unblog.fr">UnBlog</a> est hébergé chez un prestataire sur le territoire de l’Union Européenne conformément aux dispositions du Règlement Général sur la Protection des Données (RGPD : n° 2016-679)</p>
+
+<p>L’objectif est d’apporter une prestation qui assure le meilleur taux d’accessibilité. L’hébergeur assure la continuité de son service 24 Heures sur 24, tous les jours de l’année. Il se réserve néanmoins la possibilité d’interrompre le service d’hébergement pour les durées les plus courtes possibles notamment à des fins de maintenance, d’amélioration de ses infrastructures, de défaillance de ses infrastructures ou si les Prestations et Services génèrent un trafic réputé anormal.</p>
+
+<p><a href="https://unblog.fr">UnBlog</a> et l’hébergeur ne pourront être tenus responsables en cas de dysfonctionnement du réseau Internet, des lignes téléphoniques ou du matériel informatique et de téléphonie lié notamment à l’encombrement du réseau empêchant l’accès au serveur.</p>
+
+<h2>5. Propriété intellectuelle et contrefaçons.</h2>
+
+<p><a href="https://unblog.fr">UnBlog</a> est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, icônes et sons.
+    Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de : <a href="https://unblog.fr">UnBlog</a>.</p>
+
+<p>Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.</p>
+
+<h2>6. Limitations de responsabilité.</h2>
+
+<p><a href="https://unblog.fr">UnBlog</a> agit en tant qu’éditeur du site. <a href="https://unblog.fr">UnBlog</a> est responsable de la qualité et de la véracité du Contenu qu’il publie. </p>
+
+<p><a href="https://unblog.fr">UnBlog</a> ne pourra être tenu responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site internet <a href="https://unblog.fr">UnBlog</a>, et résultant soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au point 4, soit de l’apparition d’un bug ou d’une incompatibilité.</p>
+
+<p><a href="https://unblog.fr">UnBlog</a> ne pourra également être tenu responsable des dommages indirects (tels par exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du site <a href="https://unblog.fr">UnBlog</a>.
+    Des espaces interactifs (possibilité de poser des questions dans l’espace contact) sont à la disposition des utilisateurs. <a href="https://unblog.fr">UnBlog</a> se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, <a href="https://www.alibobo">UnBlog</a> se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).</p>
+
+<h2>7. Gestion des données personnelles.</h2>
+
+<p>Le Client est informé des réglementations concernant la communication marketing, la loi du 21 Juin 2014 pour la confiance dans l’Economie Numérique, la Loi Informatique et Liberté du 06 Août 2004 ainsi que du Règlement Général sur la Protection des Données (RGPD : n° 2016-679). </p>
+
+<h3>7.1 Responsables de la collecte des données personnelles</h3>
+
+<p>Pour les Données Personnelles collectées dans le cadre de la création du compte personnel de l’Utilisateur et de sa navigation sur le Site, le responsable du traitement des Données Personnelles est : dwwm3. <a href="https://unblog.fr">UnBlog</a>est représenté par contact, son représentant légal</p>
+
+<p>En tant que responsable du traitement des données qu’il collecte, <a href="https://unblog.fr">UnBlog</a> s’engage à respecter le cadre des dispositions légales en vigueur. Il lui appartient notamment au Client d’établir les finalités de ses traitements de données, de fournir à ses prospects et clients, à partir de la collecte de leurs consentements, une information complète sur le traitement de leurs données personnelles et de maintenir un registre des traitements conforme à la réalité.
+    Chaque fois que <a href="https://unblog.fr">UnBlog</a> traite des Données Personnelles, <a href="https://unblog.fr">UnBlog</a> prend toutes les mesures raisonnables pour s’assurer de l’exactitude et de la pertinence des Données Personnelles au regard des finalités pour lesquelles <a href="https://unblog.fr">UnBlog</a> les traite.</p>
+
+<h3>7.2 Finalité des données collectées</h3>
+
+<p><a href="https://unblog.fr">UnBlog</a> est susceptible de traiter tout ou partie des données : </p>
+
+<ul>
+
+    <li>pour permettre la navigation sur le Site et la gestion et la traçabilité des prestations et services commandés par l’utilisateur : données de connexion et d’utilisation du Site, facturation, historique des commandes, etc. </li>
+
+    <li>pour prévenir et lutter contre la fraude informatique (spamming, hacking…) : matériel informatique utilisé pour la navigation, l’adresse IP, le mot de passe (hashé) </li>
+
+    <li>pour améliorer la navigation sur le Site : données de connexion et d’utilisation </li>
+
+    <li>pour mener des enquêtes de satisfaction facultatives sur <a href="https://unblog.fr">UnBlog</a> : adresse email </li>
+    <li>pour mener des campagnes de communication (sms, mail) : numéro de téléphone, adresse email</li>

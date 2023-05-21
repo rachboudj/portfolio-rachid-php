cf5a7d067fe62359dd4d842c248b0a77da4b8104
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-05-21 21:08:15 +0200

    ajout page projet seul

---
+++ includes/detailProjet.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!empty($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM projets WHERE id_projet = :id_projet";
+    $query = $pdo = pdo()->prepare($sql);
+    $query->bindValue(':id_projet', $id, PDO::PARAM_INT);
+    $query->execute();
+    $projet = $query->fetch();
+    if (empty($projet)) {
+        die('Erreur 404');
+    }
+} else {
+    die('Erreur 404');
+}
+
+?>
+
+<section id="detail-projet">
+    <div class="container-detail-article">
+        <div class="img-detail-projet">
+            <img src="" alt="Projet">
+        </div>
+        <div class="text-detail-projet">
+            <h1><?php echo ucfirst($projet['titre']); ?></h1>
+            <span><?php echo ucfirst($projet['role']); ?></span>
+            <p><?php echo nl2br($projet['description']); ?></p>
+
+            <button><a href="<?php echo ucfirst($projet['url_github']); ?>">Voir le code sur GitHub</a></button>
+            <button><a href="<?php echo ucfirst($projet['url_site']); ?>">Voir le site</a></button>
+        </div>
+
+    </div>
+</section>

a5b879d2fbc27e6557d1758b531fb7849ac1aaf8
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-06-30 16:37:18 +0200

    changement lien de redirection

--- includes/supprimerCommentaire.inc.php
+++ includes/supprimerCommentaire.inc.php
@@ -5,13 +5,13 @@ if (!empty($_GET['projetId']) && ctype_digit($_GET['projetId']))
     $id = $_GET['projetId'];
     if ($pdo = pdo()) {
         $sql = "DELETE FROM commentaires WHERE commentaires.id_commentaire = '$id'";
-        // die($sql);
         $query = $pdo->query($sql);
         $query->execute();
         // header('Location: index.php?page=projetId&id='.$id);
-        // die('Problème au niveau de la redirection');
+        header('Location: index.php?page=detailProjet&id=' . $id);
+        die('Problème au niveau de la redirection');
     }
     
 } else {
-    die('Le commentaire n\'a pas été supprimé...');
+    echo('<span>Le commentaire n\'a pas été supprimé...</span>');
 }

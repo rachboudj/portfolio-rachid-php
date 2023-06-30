586f7b4abbd75f223725f95873ec683ba0bc445b
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2023-06-30 16:35:11 +0200

    changement lien de redirection

--- includes/supprimerProjet.inc.php
+++ includes/supprimerProjet.inc.php
@@ -7,9 +7,9 @@ if (!empty($_GET['projetId']) && ctype_digit($_GET['projetId']))
         $sql = "DELETE FROM projets WHERE projets.id_projet = '$id'";
         $query = $pdo->query($sql);
         $query->execute();
-        echo "<script>window.location.replace('index.php?page=projet')</script>";
+        echo "<script>window.location.replace('index.php?page=projetAdmin')</script>";
     }
     
 } else {
-    // echo('L\'article n\'a pas été supprimé...');
+    // echo('<span>Le projet n\'a pas été supprimé...</span>');
 }
